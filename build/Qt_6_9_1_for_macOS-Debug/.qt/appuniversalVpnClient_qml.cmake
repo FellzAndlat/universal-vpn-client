91574d3ead6756cae9b4d3d222c07dfad748782f
Fellz Andlat <fellzandlat@MacBook-Pro-Fellz.local>
2026-01-30 14:15:17 +0300

    update gitignore2

--- build/Qt_6_9_1_for_macOS-Debug/.qt/appuniversalVpnClient_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appuniversalVpnClient")
-set(working_dir "/Users/fellzandlat/universalVpnClient")
-set(src_and_dest_list
-    "/Users/fellzandlat/universalVpnClient/ui/Main.qml"
-    "/Users/fellzandlat/universalVpnClient/build/Qt_6_9_1_for_macOS-Debug/universalVpnClient/ui/Main.qml"
-
-)
-set(timestamp_file "/Users/fellzandlat/universalVpnClient/build/Qt_6_9_1_for_macOS-Debug/.qt/appuniversalVpnClient_qml.txt")
